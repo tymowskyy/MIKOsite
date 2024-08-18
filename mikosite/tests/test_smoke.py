fd367fe869a1c223b3628714b0ab6577b31de9c7
Karol Musieliński <musielinskikarol@gmail.com>
2024-08-18 16:33:24 +0200

    create automated tests
    New GitHub workflow was created to automatically run Django test cases
    for pull requests and pushes to main.
    
    Tests should be placed in the tests module inside the app folder. Files
    with unit tests should be named starting with 'test_'.
    
    Currently there is only one very simple test.

---
+++ mikosite/tests/test_smoke.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+class WebsiteTest(TestCase):
+
+    def test_home_page_response(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_about_response(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
+
+    def test_kolomat_response(self):
+        response = self.client.get('/kolo/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'informacje.html')
