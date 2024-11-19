fc00b2ad3785de2d80d1f00e5394dee4b9df3d16
Karol Musieliński <musielinskikarol@gmail.com>
2024-11-19 23:27:00 +0100

    implement fetching group descriptions from backend
    This commit completes implementation of dynamically fetched group
    descriptions on /kolo page. All the necessary frontend changes were
    introduced in commit 3556c46e4d50e75bdd14da69b3e2bd33844707f5.
    
    The descriptions are properly formatted e.g. every line break actually
    starts a new paragraph and `&nbsp;` behaves as expected.
    
    Please exercise some caution as any HTML tags placed in the group lead
    or description will be rendered.

--- mikosite/seminars/views.py
+++ mikosite/seminars/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
-from .models import Seminar
+from django.db.models import Q
+from django.template.defaultfilters import stringfilter
+from django.template.defaulttags import register
+from django.utils.safestring import mark_safe
+from .models import Seminar, SeminarGroup
 from babel import Locale
+
+
+@register.filter
+@stringfilter
+def split(value, key):
+    return value.split(key)
 
 
 def informacje(request):
     locale = Locale('pl_PL')
 
+    groups = (SeminarGroup.objects.all().exclude(
+        Q(lead__isnull=True) | Q(lead='') | Q(description__isnull=True) | Q(description=''))
+        .order_by('default_difficulty', 'lead'))
+
+    for group in groups:
+        group.lead = mark_safe(group.lead)
+        group.desc_snippets = [mark_safe(snippet) for snippet in group.description.split('\n') if snippet]
+
     seminars = Seminar.objects.all().order_by('date', 'time')
     for seminar in seminars:
         seminar.display = seminar.display_dict(locale)
 
-    return render(request, "informacje.html", {"seminars": seminars})
+    return render(request, "informacje.html", {"groups": groups, "seminars": seminars})
